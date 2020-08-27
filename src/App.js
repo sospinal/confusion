c9dd166ac9566636d3a7aeeee09bb029e5630100
Santiago Ospina Leal <sospinaleal@gmail.com>
2020-08-27 18:34:19 -0500

    React components part 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import {Navbar, NavbarBrand, FormGroup} from 'reactstrap';
 import Menu from './components/MenuComponent'
 import './App.css';
+import { DISHES } from './shared/dishes'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div>
@@ -15,7 +25,7 @@ class App extends Component {
             </NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
