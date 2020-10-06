8a4cb9407be3295c50ace64933cbc88bc77f7237
Santiago Ospina Leal <sospinaleal@gmail.com>
2020-10-06 14:27:01 -0500

    Assignment 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Link } from 'react-router-dom' 
+import { Link } from 'react-router-dom';
 
   function RenderDish({dish}) {
 
